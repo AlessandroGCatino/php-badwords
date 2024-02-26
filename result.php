b6b1792f8beb8fc4e9bb4b65703f2d09699cd543
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-26 15:31:46 +0100

    inserito in output paragrafo dell'utente e lunghezza stringa

--- result.php
+++ result.php
@@ -1,3 +1,10 @@
+<?php
+
+    $text = $_POST["text"];
+    $censor = $_POST["badword"];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,17 +14,26 @@
 </head>
 <body>
     <h1>
+        Il testo che hai immesso è lungo: 
+        <?= strlen($text)?> caratteri
+    </h1>
+    <h2>
+        Ecco il paragrafo che hai inserito: <br>
+    </h2>
+    <?= $text?>
+    <h1>
         Ecco il testo modificato:
     </h1>
 
     <?php
-        echo str_replace($_GET["badword"],"xxx",$_GET["text"]);
+        echo str_replace($censor ,"xxx", $text);
     ?>
     
 
 
     <style>
         h1,
+        h2,
         p{text-align: center;}
     </style>
 </body>
